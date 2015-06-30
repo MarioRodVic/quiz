57411d0cb90cea47d558c4d390ae80e44c649671
ElCosmonauta <mrv.chupito@gmail.com>
2015-06-30 10:48:00 +0200

    añadida página author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,3 +12,8 @@ exports.answer = function(req, res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+
+//GET /author
+exports.author = function(req, res){
+	res.render('author');
+};
