7294e9392e10ca724ed666695f7a114e1acbfa7a
ElCosmonauta <mrv.chupito@gmail.com>
2015-07-07 12:22:16 +0200

    proyecto adaptado a listado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,21 @@
 var models = require('../models/models.js')
 
 //GET /quizes/question
-exports.question = function(req, res){
+/*exports.question = function(req, res){
 	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/question', {pregunta: quiz[0].pregunta})	
 	})
 	
+};*/
+
+//GET /quizes/:id
+exports.show = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz:quiz});
+	})
 };
 
-//GET /quizes/answer
+/*//GET /quizes/answer
 exports.answer = function(req, res){
 	models.Quiz.findAll().then(function(quiz){
 		if(req.query.respuesta === quiz[0].respuesta){
@@ -17,6 +24,22 @@ exports.answer = function(req, res){
 			res.render('quizes/answer', {respuesta: 'Incorrecto'});
 		}
 
+	})
+};*/
+
+exports.answer= function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		if(req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto!'});
+		}else{
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto :('});
+		}
+	})
+};
+
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes})
 	})
 };
 
--- models/models.js
+++ models/models.js
@@ -37,11 +37,15 @@ exports.Quiz = Quiz; //exportar la definición de la tabla Quiz
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().then(function(){
-	//success(..) ejecuta el manejador una vez creada la tabla
+	//then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count){
 		if(count===0){    //la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de italia',
 						  respuesta: 'Roma'
+			});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
+
 			}).then(function(){console.log('Base de datos inicializada')});
 		};
 	});
