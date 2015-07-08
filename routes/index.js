f7a0d22440a6e3a97a9a66cc3636d12e4ccbbd17
ElCosmonauta <mrv.chupito@gmail.com>
2015-07-08 13:19:40 +0200

    añadido autoload para controlar errores

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-/*router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);*/
+//Autoload de comandos con :quizId
+//quizController.load() se instala para que se ejecute antes de que lo necesiten las rutas show y answer y solo en el caso de que path contenga :quizId, referenciando un recurso en la tabla Quiz de la base de datos que 
+//deba ser procesado por el controlador. Sí quizId existe en la ruta entonces llama a load
+router.param('quizId', quizController.load);
+
 
 //Definición de rutas de quizes
 router.get('/quizes', quizController.index);
