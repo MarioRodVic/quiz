b7552d47e052577ad719076ff780d554e5180f5d
ElCosmonauta <mrv.chupito@gmail.com>
2015-07-10 13:27:39 +0200

    v4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,10 +15,12 @@ exports.load = function(req, res, next, quizId){
 
 exports.index = function(req, res){
 	if(typeof(req.query.search) == "undefined"){  //comprobamos si la carga de index es a través de url preguntas o búsqueda. si existe req.query.search, entonces es que index se ha cargado a través del botón buscar
-		req.query.search="";
+		var busqueda="";
+	}else{
+		busqueda = req.query.search.toLowerCase().replace(/[ ]+/g,'%');
 	}
 	
-	models.Quiz.findAll({where: ["LOWER(pregunta) like ?", 'LOWER(%'+req.query.search+'%)']}).then(function(quizes){
+	models.Quiz.findAll({where: ["lower(pregunta) like ?", '%'+busqueda+'%'], order: 'pregunta asc'}).then(function(quizes){
 		res.render('quizes/index.ejs', {quizes: quizes, search: req.query.search});
 	}).catch(function(error){ next(error);})
 };
