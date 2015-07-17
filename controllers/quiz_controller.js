a50f39a217352be78f6e470602e5fe90586da864
ElCosmonauta <mrv.chupito@gmail.com>
2015-07-17 13:55:33 +0200

    LowerCase para las respuestas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,7 +39,7 @@ exports.show = function(req, res){
 
 exports.answer= function(req, res){
 	var resultado = 'Incorrecto';
-	if(req.query.respuesta === req.quiz.respuesta){
+	if(req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()){
 		resultado = 'Correcto!'
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors:[]});
